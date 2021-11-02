473f1ab7819c6ed9fbcf4c1c00b98822235fa2a2
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-11-02 23:36:04 +0530

    Update bee2_5.py

--- bee2_5.py
+++ bee2_5.py
@@ -51,13 +51,12 @@ while True:
     if groundx< -330:
         groundx=0
 
-    screen.blit(images["bee"],bee) 
+    screen.blit(images["bee"],bee.bee) 
     screen.blit(images["base"],[groundx,550])
     #Call the display() method of bee object to display the bee on the screen. 
       
     pygame.display.update()
     clock.tick(30)
-    
     
     
     
